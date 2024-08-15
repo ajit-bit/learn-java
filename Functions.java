a5e0f2bf115095a7d196083bbcc2bf22568b0a1d
ajit-bit <ajit1234pillai@gmail.com>
2024-08-15 11:41:26 +0530

    adding functions to inventory

---
+++ Functions.java
@@ -0,0 +1,22 @@
+import java.util.*;
+    public class Functions{
+
+
+
+        /* Print the Own input
+        public static void PrintMyName(String name){
+System.out.println(name);
+return;
+        }
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter the name ");
+            String name = sc.next();
+            PrintMyName(name);
+        }
+        /* */
+
+          
+
+    }
+   
